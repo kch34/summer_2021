ef4be9b2dda0b5e3c0ef6fa99700a122398afa87
Kevin <kch34@pitt.edu>
2021-08-03 18:01:57 -0400

    8/3/2021
    Working on using a dqn model with a custom environment

---
+++ Actual Reinforcement Learning/stable_RL.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  2 19:50:02 2021
+
+@author: Kevin Charles Hostler 
+"""
+import gym
+
+from stable_dqn import DQN
+from stable_env import SimpleMultiObsEnv
+
+
+#create the new environment
+
+#create the model from DQN
+
+#train the model
+
+#save the model
+
+
+#test the model
