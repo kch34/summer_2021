cea406826aa78f4e02eed6baff396ac03a19c5eb
Kevin <kch34@pitt.edu>
2021-08-07 08:20:23 -0400

    8/7/2021
    Working on creating a new custom env as I can't easily retool one. Along with this I might need to retool a a2c algorithm

--- Actual Reinforcement Learning/stable_RL.py
+++ Actual Reinforcement Learning/stable_RL.py
@@ -5,6 +5,9 @@ Created on Mon Aug  2 19:50:02 2021
 """
 import gym
 
+from stable_baselines3 import A2C
+
+
 from stable_env import SimpleMultiObsEnv
 from stable_baselines.common.env_checker import check_env
 
@@ -20,11 +23,31 @@ from stable_baselines.common.env_checker import check_env
 #test the model
 env = SimpleMultiObsEnv()
 
-check_env(env)
+#check_env(env)
 
+# Parallel environments
+
+model = A2C("MultiInputPolicy", env, verbose=1)
+model.learn(total_timesteps=25000)
+model.save("test")
+
+
+obs = env.reset()
+for i in range(50):
+    action, _states = model.predict(obs)
+    obs, rewards, dones, info = env.step(action)
+    env.render()
+
+
+"""
 obs = env.reset()
 n_steps = 10
 for _ in range(n_steps):
     # Random action
     action = env.action_space.sample()
     obs, reward, done, info = env.step(action)
+"""
+
+
+
+
