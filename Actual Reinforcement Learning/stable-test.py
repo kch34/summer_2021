6add03759962ab3880c9863bd23d76bb0a621086
Kevin <kch34@pitt.edu>
2021-08-07 13:09:25 -0400

    8/7/2021-2
    I might have to use open ai with open ai baselines as they have multi-agent support

--- Actual Reinforcement Learning/stable-test.py
+++ Actual Reinforcement Learning/stable-test.py
@@ -4,100 +4,73 @@ Created on Sun Aug  1 07:19:14 2021
 
 @author: Hostl
 """
-print("Test 1")
-import gym
-env = gym.make("CartPole-v1")
-# Box(4,) means that it is a Vector with 4 components
-print("Observation space:", env.observation_space)
-print("Shape:", env.observation_space.shape)
-# Discrete(2) means that there is two discrete actions
-print("Action space:", env.action_space)
-
-# The reset method is called at the beginning of an episode
-obs = env.reset()
-# Sample a random action
-action = env.action_space.sample()
-print("Sampled action:", action)
-obs, reward, done, info = env.step(action)
-# Note the obs is a numpy array
-# info is an empty dict for now but can contain any debugging info
-# reward is a scalar
-print(obs.shape, reward, done, info)
-print(" ")
-print("Test 2")
 import numpy as np
 import gym
 from gym import spaces
+from stable_baselines.common.env_checker import check_env
+from stable_baselines import DQN, PPO2, A2C, ACKTR
+from stable_baselines.common.cmd_util import make_vec_env
+from stable_env import GoLeftEnv
 
 
-class GoLeftEnv(gym.Env):
-  """
-  Custom Environment that follows gym interface.
-  This is a simple env where the agent must learn to go always left. 
-  """
-  # Because of google colab, we cannot implement the GUI ('human' render mode)
-  metadata = {'render.modes': ['console']}
-  # Define constants for clearer code
-  LEFT = 0
-  RIGHT = 1
 
-  def __init__(self, grid_size=10):
-    super(GoLeftEnv, self).__init__()
+print("Test 2")
 
-    # Size of the 1D-grid
-    self.grid_size = grid_size
-    # Initialize the agent at the right of the grid
-    self.agent_pos = grid_size - 1
 
-    # Define action and observation space
-    # They must be gym.spaces objects
-    # Example when using discrete actions, we have two: left and right
-    n_actions = 2
-    self.action_space = spaces.Discrete(n_actions)
-    # The observation will be the coordinate of the agent
-    # this can be described both by Discrete and Box space
-    self.observation_space = spaces.Box(low=0, high=self.grid_size,
-                                        shape=(1,), dtype=np.float32)
+env = GoLeftEnv()
+# If the environment don't follow the interface, an error will be thrown
+check_env(env, warn=True)
 
-  def reset(self):
-    """
-    Important: the observation must be a numpy array
-    :return: (np.array) 
-    """
-    # Initialize the agent at the right of the grid
-    self.agent_pos = self.grid_size - 1
-    # here we convert to float32 to make it more general (in case we want to use continuous actions)
-    return np.array([self.agent_pos]).astype(np.float32)
+print(" ")
+print("Testing none stable baselines")
+env = GoLeftEnv(grid_size=10)
 
-  def step(self, action):
-    if action == self.LEFT:
-      self.agent_pos -= 1
-    elif action == self.RIGHT:
-      self.agent_pos += 1
-    else:
-      raise ValueError("Received invalid action={} which is not part of the action space".format(action))
+obs = env.reset()
+env.render()
 
-    # Account for the boundaries of the grid
-    self.agent_pos = np.clip(self.agent_pos, 0, self.grid_size)
+print(env.observation_space)
+print(env.action_space)
+print(env.action_space.sample())
 
-    # Are we at the left of the grid?
-    done = bool(self.agent_pos == 0)
+GO_LEFT = 0
+# Hardcoded best agent: always go left!
+n_steps = 20
+for step in range(n_steps):
+  print("Step {}".format(step + 1))
+  obs, reward, done, info = env.step(GO_LEFT)
+  print('obs=', obs, 'reward=', reward, 'done=', done)
+  env.render()
+  if done:
+    print("Goal reached!", "reward=", reward)
+    break
 
-    # Null reward everywhere except when reaching the goal (left of the grid)
-    reward = 1 if self.agent_pos == 0 else 0
 
-    # Optionally we can pass additional info, we are not using that for now
-    info = {}
+print(" ")
+print("Testing stable baselines")
 
-    return np.array([self.agent_pos]).astype(np.float32), reward, done, info
 
-  def render(self, mode='console'):
-    if mode != 'console':
-      raise NotImplementedError()
-    # agent is represented as a cross, rest as a dot
-    print("." * self.agent_pos, end="")
-    print("x", end="")
-    print("." * (self.grid_size - self.agent_pos))
+# Instantiate the env
+env = GoLeftEnv(grid_size=10)
+# wrap it
+env = make_vec_env(lambda: env, n_envs=1)
 
-  def close(self):
-    pass
+# Train the agent
+model = ACKTR('MlpPolicy', env, verbose=1).learn(5000)
+
+# Test the trained agent
+obs = env.reset()
+n_steps = 20
+for step in range(n_steps):
+  action, _ = model.predict(obs, deterministic=True)
+  print("Step {}".format(step + 1))
+  print("Action: ", action)
+  obs, reward, done, info = env.step(action)
+  print('obs=', obs, 'reward=', reward, 'done=', done)
+  env.render(mode='console')
+  if done:
+    # Note that the VecEnv resets automatically
+    # when a done signal is encountered
+    print("Goal reached!", "reward=", reward)
+    break
+
+https://www.reddit.com/r/reinforcementlearning/comments/mcmrna/how_can_i_use_stablebaselines_to_train_multiple/
