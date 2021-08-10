c218dba4dd1183ee71cf7846628bba1ff93ac76d
Kevin <kch34@pitt.edu>
2021-08-09 22:27:20 -0400

    8/9/2021-4
    Made several changes to how openspeil is now built versus using it as a pip package

---
+++ Actual Reinforcement Learning/links.py
@@ -0,0 +1,11 @@
+#this file is for the links to openpsiel things
+
+#youtube videos
+## https://www.youtube.com/watch?v=YE0E0F39lac
+## https://www.youtube.com/watch?v=o6JNHoGUXCo
+
+#github
+## https://github.com/deepmind/open_spiel
+
+#pdf paper
+## https://arxiv.org/pdf/1908.09453.pdf
