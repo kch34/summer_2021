7f3b639c4a5f81f7b7f650b4d82d5794179ee4fa
Kevin <kch34@pitt.edu>
2021-08-11 18:29:19 -0400

    8/11/2021
    Working on getting the environment ready for tomorrow.
    
    Evidently stablebaselines3 has issues with cuda which I tried to iron out but decided it wasn't needed for the goal of the demo or completing the project

--- multi_gym/stable_test.py
+++ multi_gym/stable_test.py
@@ -7,9 +7,9 @@ Created on Tue Aug 10 12:12:45 2021
 import numpy as np
 import gym
 from gym import spaces
-from stable_baselines.common.env_checker import check_env
-from stable_baselines import DQN, PPO2, A2C, ACKTR
-from stable_baselines.common.cmd_util import make_vec_env
+from stable_baselines3.common.env_checker import check_env
+from stable_baselines3 import A2C
+from stable_baselines3.common.env_util import make_vec_env
 #import mgym
 
 from custom_env import block_world
@@ -20,16 +20,13 @@ env = GoLeftEnv(grid_size=10)
 
 
 
-
-
-
 # wrap it
 env = make_vec_env(lambda: env, n_envs=1)
 
 # Train the agent
 # robot_1 = A2C('MlpPolicy', env, verbose=1).learn(5000)
 
-robot_1 = A2C('MlpPolicy', env, verbose=1).learn(2000)
+robot_1 = A2C('MlpPolicy', env, verbose=1,device='cuda').learn(2000)
 
 # Test the trained agent
 obs = env.reset()
